9eb94d0206d0ab29d023206c886fca0d9fc75f58
miniapp <suhao@55317.com.cn>
2014-05-13 12:34:14 +0800

    add

---
+++ service/im/shs/action/IndexController.java
@@ -0,0 +1,19 @@
+package im.shs.action;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+  
+@Controller("index")
+@RequestMapping("/index")
+public class IndexController {  
+  
+    @RequestMapping(value = "/index", method = RequestMethod.GET)  
+    public String showMessage(  
+            HttpServletRequest request, HttpServletResponse response) {  
+        // TODO 处理请求  
+        return "/index"; // 设置返回页面，这里对应 /WEB-INF/view 目录下的 message.ftl 文件  
+    }
+}  

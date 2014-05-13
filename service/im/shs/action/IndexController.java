68aa5d34e9ca65ab31fbe669c943620fb0a2feb8
miniapp <suhao@55317.com.cn>
2014-05-13 09:59:26 +0800

    change succ

--- service/im/shs/action/IndexController.java
+++ service/im/shs/action/IndexController.java
@@ -6,15 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
   
-@Controller  
-@RequestMapping("/index")  
-// url映射的名称  
+@Controller("index")
+@RequestMapping("/index")
 public class IndexController {  
   
     @RequestMapping(value = "/index", method = RequestMethod.GET)  
     public String showMessage(  
             HttpServletRequest request, HttpServletResponse response) {  
         // TODO 处理请求  
-        return "/im/shs/template/index"; // 设置返回页面，这里对应 /WEB-INF/view 目录下的 message.ftl 文件  
+        return "/index"; // 设置返回页面，这里对应 /WEB-INF/view 目录下的 message.ftl 文件  
     }
 }  

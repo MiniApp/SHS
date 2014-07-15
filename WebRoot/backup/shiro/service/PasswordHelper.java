75bd694eb119935589d82c388a9e9ecfc52b73d2
MiniApp <suhao@55317.com.cn>
2014-07-16 07:56:52 +0800

    a

---
+++ WebRoot/backup/shiro/service/PasswordHelper.java
@@ -0,0 +1,47 @@
+package im.shs.base.shiro.service;
+
+import im.shs.base.shiro.entity.User;
+
+import org.apache.shiro.crypto.RandomNumberGenerator;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-28
+ * <p>Version: 1.0
+ */
+@Service("passwordHelper")
+public class PasswordHelper {
+
+    private RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
+    private String algorithmName = "md5";
+    private int hashIterations = 2;
+
+    public void setRandomNumberGenerator(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
+
+    public void setAlgorithmName(String algorithmName) {
+        this.algorithmName = algorithmName;
+    }
+
+    public void setHashIterations(int hashIterations) {
+        this.hashIterations = hashIterations;
+    }
+
+    public void encryptPassword(User user) {
+
+        user.setSalt(randomNumberGenerator.nextBytes().toHex());
+
+        String newPassword = new SimpleHash(
+                algorithmName,
+                user.getPassword(),
+                ByteSource.Util.bytes(user.getUsername() + user.getSalt()),
+                hashIterations).toHex();
+
+        user.setPassword(newPassword);
+    }
+}
